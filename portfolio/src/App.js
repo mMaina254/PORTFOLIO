aced12639c9d53f97d3a9693a03da4f414683d19
Maina Michael <mainamichael1000@gmail.com>
2026-06-19 13:30:05 +0300

    imported the header component from Header.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div id="app" className="App">
-        Hello World
+      <Header />
     </div>
   );
 }
